f7db59c245ee11ce9d9ee02f63c0da9d9948a2e5
actionpterygii <actionpterygii@gmail.com>
2020-10-01 00:47:34 +0900

    それによってカテゴリーページもいい感じね

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,29 +4,79 @@ import {useParams} from "react-router-dom";
 import useAPI from "../scripts/useAPI";
 
 function Category() {
-  const {blogCategory, fetchBlogCategory} = useAPI();
+  const {
+    blogCategory,
+    fetchBlogCategory,
+    blogPostList,
+    fetchBlogPostList,
+    postIdToPath
+  } = useAPI();
   const {key} = useParams();
 
   useEffect(() => {
     fetchBlogCategory(key);
+    fetchBlogPostList("new", "category", key);
   }, []);
 
-  if (blogCategory.fetching) {
-    return <div>よみこみちゅうです</div>;
-  } else {
-    if (blogCategory.success) {
-      return (
+  return (
+    <div>
+      {blogCategory.fetching ? (
+        <div>よみこみちゅうです</div>
+      ) : blogCategory.success ? (
         <div>
           <h2>{blogCategory.name}</h2>
           <div>{blogCategory.desc}</div>
+          <div>
+            {blogPostList.fetching ? (
+              <div>よみこみちゅう</div>
+            ) : blogPostList.success ? (
+              <div>
+                <h3>記事一覧</h3>
+                <ul>
+                  {blogPostList.contents.map((blogPost, i) => (
+                    <li key={i}>
+                      <a href={`/#/post/${postIdToPath(blogPost.id)}`}>
+                        <h4>{blogPost.title}</h4>
+                        <div>{blogPost.content}</div>
+                        <div>{blogPost.category.name}</div>
+                        <ul>
+                          {blogPost.tags.map((tag, i) => (
+                            <li key={i}>{tag.name}</li>
+                          ))}
+                        </ul>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+                <div>
+                  {blogPostList.moreFetching ? (
+                    <div>読み込み中</div>
+                  ) : blogPostList.totalCount > blogPostList.contents.length ? (
+                    <button
+                      type="button"
+                      onClick={() => fetchBlogPostList("continue")}
+                    >
+                      更に読み込む
+                    </button>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            ) : blogPostList.failed ? (
+              <div>ないです</div>
+            ) : (
+              <div>なにか普通じゃない状態です</div>
+            )}
+          </div>
         </div>
-      );
-    } else if (blogCategory.failed) {
-      return <div>ないです</div>;
-    } else {
-      return <div>なにか普通じゃない状態です。</div>;
-    }
-  }
+      ) : blogCategory.failed ? (
+        <div>ないです</div>
+      ) : (
+        <div>なにか普通じゃない状態です。</div>
+      )}
+    </div>
+  );
 }
 
 export default Category;
