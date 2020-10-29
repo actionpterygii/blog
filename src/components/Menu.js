aaa5a204e08a92b33a821f78c4c91e5950404867
actionpterygii <actionpterygii@gmail.com>
2020-10-29 20:39:14 +0900

    けってい

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,9 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import {PATH} from "../config";
-
 function Menu() {
   return (
     <div>
-      {/* <div>
-        <a href={`${PATH}post`}>記事一覧</a>
-      </div>
-      <div>
-        <a href={`${PATH}search`}>記事検索</a>
-      </div>
-      <div>
-        <a href={`${PATH}category`}>カテゴリ一覧</a>
-      </div>
-      <div>
-        <a href={`${PATH}tag`}>タグ一覧</a>
-      </div> */}
       <div>
         <Link to="/">記事一覧</Link>
       </div>
